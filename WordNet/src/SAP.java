2790212e7effbc072db18877c0b2a9bef02355dc
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-25 14:10:51 -0700

    added SAP api

--- WordNet/src/SAP.java
+++ WordNet/src/SAP.java
@@ -5,4 +5,23 @@ import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdIn;
 
 public class SAP {
+    public SAP(Digraph G){
+
+    }
+
+    public int length(int v, int w){
+        return 0;
+    }
+
+    public int ancestor(int v, int w){
+        return 0;
+    }
+
+    public int length(Iterable<Integer> v, Iterable<Integer> w){
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
