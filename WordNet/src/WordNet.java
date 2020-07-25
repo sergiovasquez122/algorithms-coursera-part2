fd9c8fa7446fee6b397a37c93139a83a35929758
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-25 14:30:57 -0700

    added comments

--- WordNet/src/WordNet.java
+++ WordNet/src/WordNet.java
@@ -1,21 +1,31 @@
 import edu.princeton.cs.algs4.*;
 
 public class WordNet {
+
+    /**
+     * Constructor that takes the name of two input files
+     * @param synsets the file contains all noun synsets in WordNet
+     * @param hypernyms the file contains all hypernym relationships
+     */
     public WordNet(String synsets, String hypernyms){
         if(synsets == null || hypernyms == null){
             throw new IllegalArgumentException();
         }
 
-        In synset_file = new In(synsets);
-        while(!synset_file.isEmpty()){
-            String[] a = synset_file.readLine().split(",");
-        }
     }
 
+    /**
+     *
+     * @return all WordNetNouns
+     */
     public Iterable<String> nouns(){
         return null;
     }
 
+    /**
+     * @param word a nonnull String
+     * @return true if the word is a noun in the synset false otherwise
+     */
     public boolean isNoun(String word){
         if(word == null){
             throw new IllegalArgumentException();
@@ -23,6 +33,11 @@ public class WordNet {
         return false;
     }
 
+    /**
+     * @param nounA
+     * @param nounB
+     * @return distance between nounA and nounB in the Wordnet Graph
+     */
     public int distance(String nounA, String nounB){
         if(!isNoun(nounA) || !isNoun(nounB)){
             throw new IllegalArgumentException();
@@ -30,12 +45,19 @@ public class WordNet {
         return 0;
     }
 
+    /**
+     * @param nounA
+     * @param nounB
+     * @return a synset that is the common ancestor two nounA and nounB in a shortest
+     * ancestral path
+     */
     public String sap(String nounA, String nounB){
         if(nounA == null || nounB == null){
             throw new IllegalArgumentException();
         }
         return "";
     }
+
 
     public static void main(String[] args) {
         WordNet wordNet = new WordNet("/home/sergio/algorithms-coursera-part2/WordNet/src/synsets.txt", "");
