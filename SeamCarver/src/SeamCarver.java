72d3729a79202986fc7d7943ec8fe3a901d72d0a
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-26 13:38:05 -0700

    added exceptions throwing

--- SeamCarver/src/SeamCarver.java
+++ SeamCarver/src/SeamCarver.java
@@ -1,47 +1,57 @@
 import edu.princeton.cs.algs4.Picture;
-import edu.princeton.cs.algs4.Stack;
-import edu.princeton.cs.algs4.StdOut;
 
 import java.awt.Color;
 
 public class SeamCarver {
-    public SeamCarver(Picture picture){
+    private Picture picture;
+    private int width;
+    private int height;
 
+    public SeamCarver(Picture picture){
+        this.picture = picture;
+        this.height = picture.height();
+        this.width = picture.width();
     }
 
     public Picture picture(){
-
+        return this.picture;
     }
 
     public int width(){
-
+        return width;
     }
 
     public int height(){
-
+        return height;
     }
 
     public double energy(int x, int y){
-
+        if(x < 0 || x >= width() || y < 0 || y >= height()){
+            throw new IllegalArgumentException();
+        }
+        return 0;
     }
 
     public int[] findHorizontalSeam(){
-
+        return null;
     }
 
     public int[] findVerticalSeam(){
-
+        return null;
     }
 
     public void removeHorizontalSeam(int[] seam){
-
+        if(seam == null){
+            throw new IllegalArgumentException();
+        }
     }
 
     public void removeVerticalSeam(int[] seam){
-
+        if(seam == null){
+            throw new IllegalArgumentException();
+        }
     }
 
     public static void main(String[] args) {
-        g
     }
 }
