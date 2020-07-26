d4ef8a29158d789151e14924a93584d584c6fc79
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-26 14:08:11 -0700

    added helper method

--- SeamCarver/src/SeamCarver.java
+++ SeamCarver/src/SeamCarver.java
@@ -1,6 +1,7 @@
 import edu.princeton.cs.algs4.Picture;
 
 import java.awt.Color;
+import java.util.Collections;
 
 public class SeamCarver {
     private Picture picture;
@@ -14,7 +15,7 @@ public class SeamCarver {
     }
 
     public Picture picture(){
-        return new Picture(this.picture);
+        return picture;
     }
 
     public int width(){
@@ -29,7 +30,19 @@ public class SeamCarver {
         if(x < 0 || x >= width() || y < 0 || y >= height()){
             throw new IllegalArgumentException();
         }
-        return 0;
+        // boundary entries have an energy function of 1000
+        if(x == 0 || x == width - 1 || y == 0 || y == height - 1){
+            return 1000;
+        }
+    }
+
+    private double calculateXGradient(int x, int y){
+        Color c1 = picture.get(x - 1, y);
+        Color c2 = picture.get(x + 1, y);
+        double redDiff = Math.pow(c1.getRed() - c2.getRed(), 2);
+        double blueDiff = Math.pow(c1.getBlue() - c2.getBlue(), 2);
+        double greenDiff = Math.pow(c1.getGreen() - c2.getGreen(), 2);
+        return redDiff + blueDiff + greenDiff;
     }
 
     public int[] findHorizontalSeam(){
