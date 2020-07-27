bb7c222b5348f843f5350d517d7ebcd162e31cec
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-27 16:14:55 -0700

    added BoggleSolver api

--- Boggle/src/BoggleSolver.java
+++ Boggle/src/BoggleSolver.java
@@ -4,4 +4,15 @@ import edu.princeton.cs.algs4.StdOut;
 
 public class BoggleSolver {
 
+    public BoggleSolver(String[] dictionary){
+
+    }
+
+    public Iterable<String> getAllValidWords(BoggleBoard board){
+        return null;
+    }
+
+    public int scoreOf(String word){
+        return 0;
+    }
 }
