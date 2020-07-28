4ec5ded5839a69bc9856a4fd1ef4e61cce86100e
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-27 17:15:30 -0700

    added implementation of boggle board solver

--- Boggle/src/BoggleSolver.java
+++ Boggle/src/BoggleSolver.java
@@ -5,7 +5,7 @@ import edu.princeton.cs.algs4.StdOut;
 public class BoggleSolver {
 
     private TrieSET ts;
-    private TrieSET words;
+    private SET<String> words;
     private boolean[][] marked;
     BoggleBoard b;
     /**
@@ -15,7 +15,7 @@ public class BoggleSolver {
      */
     public BoggleSolver(String[] dictionary){
         ts = new TrieSET();
-        words = new TrieSET();
+        words = new SET<>();
         for(String s : dictionary)
             ts.add(s);
     }
@@ -31,19 +31,21 @@ public class BoggleSolver {
                 dfs("", i, j);
             }
         }
-        return words.keys();
+        return words;
     }
 
     private void dfs(String pre, int x, int y){
-        marked[x][y] = true;
-        pre += b.getLetter(x, y) == 'Q' ? "Qu"  : b.getLetter(x, y);
-        if(pre.length() > 2 && ts.contains(pre)) words.add(pre);
+        if(marked[x][y]) return;
+        char letter = b.getLetter(x,y);
+        pre = pre+ (letter=='Q' ? "QU" : letter);
+        if(pre.length()>2 && ts.contains(pre)) words.add(pre);
         if(!ts.isPrefix(pre)) return;
         int[][] positions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
+        marked[x][y] = true;
         for(int[] pos : positions){
-            int new_x = pos[0];
-            int new_y = pos[1];
-            if(new_x >= 0 && new_x < b.rows() && new_y >= 0 && new_y < b.cols() && !marked[new_x][new_y]){
+            int new_x = pos[0] + x;
+            int new_y = pos[1] + y;
+            if(new_x >= 0 && new_x < b.rows() && new_y >= 0 && new_y < b.cols()){
                 dfs(pre, new_x, new_y);
             }
         }
@@ -54,13 +56,15 @@ public class BoggleSolver {
     // Returns the score of the given word if it is in the dictionary, zero otherwise.
     // (You can assume the word contains only the uppercase letters A through Z.)
     public int scoreOf(String word){
-        if(!ts.contains(word) || word.length() <= 2) return 0;
-        int l = word.length();
-        int add_one = word.charAt(0) == 'Q' ? 1 : 0;
-        if( l == 3 || l == 4 ) return 1 + add_one;
-        else if( l == 6 ) return 3 + add_one;
-        else if( l == 7 ) return 5 + add_one;
-        else return 11 + add_one;
+        if(ts.contains(word)){
+            int l = word.length();
+            if(l == 3 || l == 4)return 1;
+            else if(l==5) return 2;
+            else if(l==6) return 3;
+            else if(l==7) return 5;
+            else if(l>=8) return 11;
+        }
+        return 0;
     }
 
 
